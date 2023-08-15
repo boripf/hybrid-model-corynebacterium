197bf6a4cc7399e5d97a744a1427df1b40b0f4d0
boripf <pfaff.deborah@gmail.com>
2023-08-15 16:13:46 +0200

    calculating rmse for batch no2 - no 2 phases

--- B_model.py
+++ B_model.py
@@ -305,3 +305,110 @@ def objective_function_no2(delta_t):
     mse = (mse_x + mse_s + mse_co2) / 3
     rmse = np.sqrt(mse)  # Calculate root mean squared error
     return rmse
+
+####-------------------------------------------------------------------------------------------------
+# BATCH NO. 2 - without segmentation
+
+def objective_function_no2_est(parameters):
+    # Solve the model using the optimal parameters
+    time_pred, biomass_pred, substrate_pred, co2_pred = model_no2_est(parameters)  # Solve the model using the current parameters
+    biomass = pd.concat([biomass_exp_2, pd.Series(biomass_pred)], axis=1, keys=['biomass_exp', 'biomass_pred']).dropna()
+    biomass_exp_ = biomass['biomass_exp'].values
+    biomass_pred_ = biomass['biomass_pred'].values
+    mse_x = mean_squared_error(biomass_exp_, biomass_pred_)  # Calculate mean squared error for biomass
+
+    glucose = pd.concat([substrate_exp_2, pd.Series(substrate_pred)], axis=1, keys=['substrate_exp', 'substrate_pred']).dropna()
+    substrate_exp_ = glucose['substrate_exp'].values
+    substrate_pred_ = glucose['substrate_pred'].values
+    mse_s = mean_squared_error(substrate_exp_, substrate_pred_)  # Calculate mean squared error for substrate
+
+    co2 = pd.concat([co2_exp_2, pd.Series(co2_pred)], axis=1, keys=['co2_exp', 'co2_pred']).dropna()
+    co2_exp_ = co2['co2_exp'].values
+    co2_pred_ = co2['co2_pred'].values
+    mse_co2 = mean_squared_error(co2_exp_, co2_pred_)  # Calculate mean squared error for substrate
+    
+    # Calculate the combined rmse
+    mse = (mse_x + mse_s + mse_co2) / 3
+    rmse = np.sqrt(mse)  # Calculate root mean squared error
+    return rmse, time_pred, biomass_pred, substrate_pred, co2_pred
+
+def model_no2_est(parameters):
+    """
+    Args:
+        parameters (list): List containing the model parameters.
+
+    Returns:
+        time (array): Array of time values.
+        biomass (array): Array of biomass concentrations.
+        substrate (array): Array of substrate concentrations.
+        co2 (array): Array of co2 concentrations.
+    """
+
+    # Simulation settings
+    t0 = 0
+    t_end = 36.8
+    delta_t = 1
+    dt = delta_t/60
+    num_steps = int((t_end - t0) / dt) + 1 # Number of time steps
+
+    # Initial values
+    X0 = param['X0_2']
+    S0 = param['S0']
+    co20 = param['co20']
+    V0 = param['V0_2']
+    c_glu_feed = param['c_glu_feed']
+
+    # Set parameters from input
+    Yxs = parameters[0]
+    Yco2s = parameters[1]
+    qs_max = parameters[2]
+    Ks = parameters[3]
+    m_s = parameters[4]
+    lag = parameters[5]
+
+    # Arrays to store results
+    time = np.linspace(t0, t_end, num_steps)
+    biomass = np.zeros(num_steps)
+    substrate = np.zeros(num_steps)
+    co2 = np.zeros(num_steps)
+    volume = np.zeros(num_steps)
+
+    # Set initial values
+    biomass[0] = X0
+    substrate[0] = S0
+    co2[0] = co20
+    volume[0] = V0
+
+    # Simulation loop
+    for i in range(1, num_steps):
+        c_glucose = substrate[i-1]
+        c_biomass = biomass[i-1]
+        c_co2 = co2[i-1]
+        vol = volume[i-1]
+
+        # time steps need to be adapted for experimental data input
+        t = i * delta_t
+        f_glucose = F_glu_2[t]
+        f_total = F_in_2[t]
+        
+        # Update growth and glucose uptake rate
+        qs = qs_max * c_glucose / (Ks + c_glucose) * (1 / (np.exp(c_biomass * lag)))
+        mu = qs * Yxs
+
+        # Update biomass and substrate concentrations
+        dV_dt = f_total - (0.4*60/num_steps) # [L/h] not complete -- include samples + evaporation
+        dX_dt = mu * c_biomass - (c_biomass / vol) * dV_dt # [gx/(Lh)]
+        dS_dt = ((f_glucose / vol) * (c_glu_feed - c_glucose)) - ((qs + m_s) * c_biomass) - ((c_glucose / vol) * dV_dt)
+        # [gs/(Lh)]
+        dCO2_dt = Yco2s * qs * c_biomass  - (c_co2 * (dV_dt / vol)) # [g/(Lh)]
+
+        biomass[i] = c_biomass + dX_dt * dt # [gx/L]
+        substrate[i] = c_glucose + dS_dt * dt # [gs/L]
+        co2[i] = c_co2 + dCO2_dt * dt  # [g/L]
+        volume[i] = vol + dV_dt * dt # [L]
+
+        # since the glucose concentration can't be negative, it is set to zero
+        if substrate[i] < 0:
+            substrate[i] = 0
+
+    return time, biomass, substrate, co2
